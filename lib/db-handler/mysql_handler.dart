18eef2364926caaef6790cb7d4418f761f7d136d
NevrCode <kevin12keval@gmail.com>
2024-01-10 19:44:18 +0700

    benerin tampilan

--- lib/db-handler/mysql_handler.dart
+++ lib/db-handler/mysql_handler.dart
@@ -1,18 +1,14 @@
 import 'package:mysql1/mysql1.dart';
 
 class MySQLHandler {
+  static Future<MySqlConnection> mySQLOpenDB() async {
+    final settings = ConnectionSettings(
+        host: '192.168.100.71', port: 3306, user: 'root', db: 'lib_app_db');
 
-static Future<MySqlConnection> mySQLOpenDB() async {
-  final settings =  ConnectionSettings(
-  host: '10.0.2.2', 
-  port: 3306,
-  user: 'root',
-  db: 'lib_app_db');
+    return await MySqlConnection.connect(settings);
+  }
 
-  return await MySqlConnection.connect(settings); 
-}
-static void closeMySQLDB(MySqlConnection conn) async {
-  return conn.close();
-}
-
+  static void closeMySQLDB(MySqlConnection conn) async {
+    return conn.close();
+  }
 }
